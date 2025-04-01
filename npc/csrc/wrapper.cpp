3d9efa57895c59e1c9fe0dbd1aefb7fa73c762b8
3022232140-yubo_cui <yuboiverson@gmail.com>
2025-04-01 13:20:13 +0000

    streaming light in npc

--- npc/csrc/wrapper.cpp
+++ npc/csrc/wrapper.cpp
@@ -1,10 +1,40 @@
 // #include "verilated_fst_c.h"
-#include "Vtop.h"
+#include "Vlight.h"
 #include "verilated.h"
 #include <nvboard.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #define MAX_TIME 100
 
+TOP_NAME *top;
 void nvboard_bind_all_pins(TOP_NAME *top);
+
+void single_cycle()
+{
+  top->clk = 0;
+  top->eval();
+  nvboard_update();
+  top->clk = 1;
+  top->eval();
+  nvboard_update();
+}
+
+// Reset the design for n cycles
+void reset(int n)
+{
+  top->rst = 1;
+  while (n-- > 0)
+    single_cycle();
+  top->rst = 0;
+}
+
+// ... reset(10); // reset for 10 cycles
+// while(???)
+// {
+//   ... single_cycle();
+//   ...
+// }
 
 int main(int argc, char **argv)
 {
@@ -18,35 +48,14 @@ int main(int argc, char **argv)
   // int sim_time = 0;
   nvboard_bind_all_pins(top);
   nvboard_init();
+  reset(10); // reset for 10 cycles
   while (!contextp->gotFinish())
   {
-    top->a = 0;
-    top->b = 0;
-    top->eval();
-    // tfp->dump(sim_time);
-    nvboard_update();
-    // sim_time++;
-    top->a = 0;
-    top->b = 0;
-    top->eval();
-    // tfp->dump(sim_time);
-    nvboard_update();
-    // sim_time++;
-    top->a = 0;
-    top->b = 1;
-    top->eval();
-    // tfp->dump(sim_time);
-    nvboard_update();
-    // sim_time++;
-    top->a = 0;
-    top->b = 1;
-    top->eval();
-    nvboard_update();
-    // tfp->dump(sim_time);
-    // sim_time++;
+    single_cycle();
   }
   // tfp->close();
   // delete tfp;
+  nvboard_quit();
   delete top;
   delete contextp;
   return 0;
